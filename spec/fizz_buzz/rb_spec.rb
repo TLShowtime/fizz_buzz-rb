b29ecf2f248bbf3cd2ea2dd306c855cf97a99209
TLShowtime <liuguo35@hotmail.com>
2022-12-13 17:10:25 -0600

    Plain numbers implemented with tests

--- spec/fizz_buzz/rb_spec.rb
+++ spec/fizz_buzz/rb_spec.rb
@@ -1,9 +1,17 @@
-RSpec.describe FizzBuzz::Rb do
-  it "has a version number" do
-    expect(FizzBuzz::Rb::VERSION).not_to be nil
+RSpec.describe FizzBuzz::Game do
+  it "see plain_numbers(1,4)" do
+    expect(FizzBuzz::Game.new.display_plain_number(1, 4)).to eq("1,2,3,4")
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "see plain_numbers(10,15)" do
+    expect(FizzBuzz::Game.new.display_plain_number(10, 15)).to eq("10,11,12,13,14,15")
+  end
+
+  it "see plain_numbers('2',12)" do
+    expect(FizzBuzz::Game.new.display_plain_number('2', 12)).to eq("Not accepted numbers")
+  end
+
+  it "see plain_numbers(2, 23.2)" do
+    expect(FizzBuzz::Game.new.display_plain_number(2, 23.2)).to eq("Not accepted numbers")
   end
 end
