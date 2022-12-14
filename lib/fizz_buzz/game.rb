5cf4377572dc761c729ca0578355a8fc648abd3f
Jose Alvarez <alvarezloaiciga@gmail.com>
2022-12-14 09:33:26 -0600

    Rspec changes

--- lib/fizz_buzz/game.rb
+++ lib/fizz_buzz/game.rb
@@ -1,5 +1,6 @@
 module FizzBuzz
   class Game
+
     def display_plain_number(first_number, last_number)
       if !validate_numbers(first_number, last_number)
         "Not accepted numbers"
@@ -14,6 +15,8 @@ module FizzBuzz
         number_series
       end
     end
+
+    private
 
     def validate_numbers(first_number, last_number)
       first_number.is_a?(Integer) && last_number.is_a?(Integer)
--- spec/fizz_buzz/game_spec.rb
+++ spec/fizz_buzz/game_spec.rb
@@ -3,15 +3,19 @@
 require "spec_helper"
 
 RSpec.describe FizzBuzz::Game do
-  it "see plain_numbers(10,15)" do
-    expect(FizzBuzz::Game.new.display_plain_number(10, 15)).to eq("10,11,12,13,14,15")
-  end
+  describe "#plain_numbers" do
+    subject(:game) { described_class.new }
 
-  it "see plain_numbers('2',12)" do
-    expect(FizzBuzz::Game.new.display_plain_number('2', 12)).to eq("Not accepted numbers")
-  end
+    it "displays numbers from 10 to 15" do
+      expect(game.display_plain_number(10, 15)).to eq("10,11,12,13,14,15")
+    end
 
-  it "see plain_numbers(2, 23.2)" do
-    expect(FizzBuzz::Game.new.display_plain_number(2, 23.2)).to eq("Not accepted numbers")
+    it "does not accept strings as paramaters" do
+      expect(game.display_plain_number('2', 12)).to eq("Not accepted numbers")
+    end
+
+    it "does not accept float as parameters" do
+      expect(game.display_plain_number(2, 23.2)).to eq("Not accepted numbers")
+    end
   end
 end
