7508b23840ebc5916e73b2b44a70a8e1e7758756
TLShowtime <liuguo35@hotmail.com>
2022-12-14 16:55:49 -0600

    Fizzbuzz implemented

--- lib/fizz_buzz/game.rb
+++ lib/fizz_buzz/game.rb
@@ -9,6 +9,24 @@ module FizzBuzz
       end
     end
 
+    def display_fizzbuzz(first_number, last_number)
+      if !validate_numbers(first_number, last_number)
+        "Not accepted numbers"
+      else
+        (first_number..last_number).map{ |number|
+          if number % 3 == 0 && number % 5 == 0
+            "fizzbuzz"
+          elsif number % 3 == 0
+            "fizz"
+          elsif number % 5 == 0
+            "buzz"
+          else
+            number.to_s
+          end
+        }.join(",")
+      end
+    end
+
     private
 
     def validate_numbers(first_number, last_number)
