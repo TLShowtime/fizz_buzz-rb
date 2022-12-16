079746b1f85dce1a9cc45290883273899cb3d2a6
TLShowtime <liuguo35@hotmail.com>
2022-12-16 10:09:16 -0600

    Rubocop implemented

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative "fizz_buzz/version"
-require_relative "fizz_buzz/game"
+require_relative 'fizz_buzz/version'
+require_relative 'fizz_buzz/game'
 
 module FizzBuzz
   class Error < StandardError; end
--- lib/fizz_buzz/game.rb
+++ lib/fizz_buzz/game.rb
@@ -1,40 +1,40 @@
-module FizzBuzz
-  class Game
+# frozen_string_literal: true
 
+module FizzBuzz
+  # Game for fizzbuzz
+  class Game
     def display_plain_number(first_number, last_number)
       if !validate_numbers(first_number, last_number)
-        "Not accepted numbers"
+        'Not accepted numbers'
       else
-        (first_number..last_number).to_a.join(",")
+        (first_number..last_number).to_a.join(',')
       end
     end
 
     def display_fizzbuzz(first_number, last_number)
       if !validate_numbers(first_number, last_number)
-        "Not accepted numbers"
+        'Not accepted numbers'
       else
-        (first_number..last_number).map{ |number|
+        (first_number..last_number).map do |number|
           show_value_fizzbuzz(number)
-        }.join(",")
+        end.join(',')
       end
     end
 
     def play
       keep_playing = true
       while keep_playing
-        puts "Enter first number: "
+        puts 'Enter first number: '
         first_number = gets.chomp
-        puts "Enter second number: "
+        puts 'Enter second number: '
         second_number = gets.chomp
-        if not validate_user_numbers(first_number, second_number)
-          puts "Invalid input"
+        if !validate_user_numbers(first_number, second_number)
+          puts 'Invalid input'
         else
           puts display_fizzbuzz(first_number.to_i, second_number.to_i)
         end
         puts "Type 'exit' to exit, otherwise enter any input"
-        if gets.chomp == "exit"
-          keep_playing = false
-        end
+        keep_playing = false if gets.chomp == 'exit'
       end
     end
 
@@ -45,17 +45,16 @@ module FizzBuzz
     end
 
     def show_value_fizzbuzz(number)
-      value = ""
-      value = "fizz" if number % 3 == 0
-      value += "buzz" if number % 5 == 0
+      value = ''
+      value = 'fizz' if (number % 3).zero?
+      value += 'buzz' if (number % 5).zero?
 
-      value == "" ? number : value
+      value == '' ? number : value
     end
 
     def validate_user_numbers(first_number, second_number)
       regex = /^(\d)+$/
       (first_number.match? regex) && (second_number.match? regex)
     end
-
   end
 end
