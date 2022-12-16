6a72563ddf8c097e5982704408d33650fbebf5a5
TLShowtime <liuguo35@hotmail.com>
2022-12-16 08:41:27 -0600

    Changed to bundle exec rspec

--- spec/fizz_buzz/game_spec.rb
+++ spec/fizz_buzz/game_spec.rb
@@ -42,8 +42,49 @@ RSpec.describe FizzBuzz::Game do
   describe "#play" do
     subject(:game) { described_class.new }
 
-    it "play fizzbuzz from 4 to 15" do
-      expect(game.play)
+    it "play fizzbuzz from 4 to 15, then exit" do
+      allow(game).to receive(:gets).and_return("4","15","exit")
+      expect{ game.play }.to output(
+        "Enter first number: \n" +
+        "Enter second number: \n" +
+        "4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzbuzz\n" +
+        "Type 'exit' to exit, otherwise enter any input\n"
+      ).to_stdout
     end
+
+    it "play fizzbuzz from 1 to 5, then play from 3 to 6 then exit" do
+      allow(game).to receive(:gets).and_return("1","5","\n","3","6","exit")
+      expect{ game.play }.to output(
+        "Enter first number: \n" +
+        "Enter second number: \n" +
+        "1,2,fizz,4,buzz\n" +
+        "Type 'exit' to exit, otherwise enter any input\n" +
+        "Enter first number: \n" +
+        "Enter second number: \n" +
+        "fizz,4,buzz,fizz\n" +
+        "Type 'exit' to exit, otherwise enter any input\n"
+      ).to_stdout
+    end
+
+    it "does not accept string values, then exit" do
+      allow(game).to receive(:gets).and_return("a","5","exit")
+      expect{ game.play }.to output(
+        "Enter first number: \n" +
+        "Enter second number: \n" +
+        "Invalid input\n" +
+        "Type 'exit' to exit, otherwise enter any input\n"
+      ).to_stdout
+    end
+
+    it "does not accept float values, then exit" do
+      allow(game).to receive(:gets).and_return("5","4.1","exit")
+      expect{ game.play }.to output(
+        "Enter first number: \n" +
+        "Enter second number: \n" +
+        "Invalid input\n" +
+        "Type 'exit' to exit, otherwise enter any input\n"
+      ).to_stdout
+    end
+
   end
 end
